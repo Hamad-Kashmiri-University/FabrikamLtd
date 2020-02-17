03f95e3d27214363201d24f49f3a1c355af5c0f1
Hamad Kashmiri <kashmirh@uni.coventry.ac.uk>
2020-02-17 16:24:13 +0000

    homepage routing done

--- EnterpriseProject/urls.py
+++ EnterpriseProject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('bookings/', include('Bookings.urls')),
 ]
