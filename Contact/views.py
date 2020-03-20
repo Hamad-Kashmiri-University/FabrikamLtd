4393fe358c4efea81ca1956ee9808c3f00ebbb31
campb167 <finish@live.co.uk>
2020-03-20 16:00:27 +0000

    Created Contact App Folder

--- Contact/views.py
+++ Contact/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(request):
-    return render(request, 'home/index.html')
-
-def about(request):
-    return render(request, 'home/about.html')
+# Create your views here.
