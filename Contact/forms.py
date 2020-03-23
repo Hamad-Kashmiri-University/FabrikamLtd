9ac9a6db8a9c783153a8e395938ba165877161ab
campb167 <finish@live.co.uk>
2020-03-23 15:16:33 +0000

    Changed form to custom template + now emails

--- Contact/forms.py
+++ Contact/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from Contact.models import suppTicket
 
 class suppTicketForm(forms.ModelForm):
+    name = forms.CharField( widget=forms.TextInput(attrs={'class': "form-control"}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': "form-control",'id': "clientemail"}))
+    message = forms.CharField( widget=forms.Textarea(attrs={'class': "form-control"}))
+    subject = forms.CharField( widget=forms.TextInput(attrs={'class': "form-control"}))
     class Meta:
         model = suppTicket
         fields = ['name', 'email', 'subject', 'message',]
--- Contact/views.py
+++ Contact/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from Contact.forms import suppTicketForm
 from Contact.models import suppTicket
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 def contact(request):
     form = suppTicketForm(request.POST)
     if request.method == "POST":
         if form.is_valid():
             form.save()
-            return redirect("Home")
+            email = request.POST.get('email')
+            name = request.POST.get('name')
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [email]
+            content = (message + " from " + name + " reply to " + email) 
+            email = EmailMessage(subject,content,email_from,recipient_list)
+            email.send()
+            return redirect("Contact")
     else:
         return render(request, 'Contact/contact.html', {'form': form})
 
