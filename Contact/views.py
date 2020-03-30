f6247e6d12db2d4c456c1a658dd9c53e8d21e1f1
campb167 <finish@live.co.uk>
2020-03-30 19:09:14 +0100

    emails to user email confirmation of ticket.

--- Contact/views.py
+++ Contact/views.py
@@ -17,10 +17,16 @@ def contact(request):
             subject = request.POST.get('subject')
             message = request.POST.get('message')
             email_from = settings.EMAIL_HOST_USER
+            user_email = [email]
             recipient_list = [settings.EMAIL_HOST_USER]
             content = (message + " from " + name + " reply to " + email) 
             email = EmailMessage(subject,content,email_from,recipient_list)
             email.send()
+            subject = ("Support Ticket")
+            content = ("Your support ticket has been recieved, we will be in touch shortly!")
+            email = EmailMessage(subject, content,email_from,user_email)
+            email.send()
+
             return redirect("Contact")
     else:
         return render(request, 'Contact/contact.html', {'form': form})
